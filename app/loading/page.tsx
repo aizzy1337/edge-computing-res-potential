6f8c28f20b23e03db9625979df09e46a97323fe8
Arkadiusz Zając <arekzaj7@gmail.com>
2024-09-09 19:07:36 +0200

    Fixes in deployment

--- app/loading/page.tsx
+++ app/loading/page.tsx
@@ -1,13 +1,26 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+import loadingProps from '../types/loading-props';
+
+function Location(): loadingProps {
+    const searchParams = useSearchParams();
+    let location: loadingProps = { lat: 0, lng: 0 };
+
+    if(searchParams.get('lat') != null && searchParams.get('lng') != null) {
+        location = {
+            lat: parseFloat(searchParams.get('lat') as string),
+            lng: parseFloat(searchParams.get('lng') as string),
+        }   
+    }
+    return location;
+}
 
 export default function Loading() {
-    const searchParams = useSearchParams();
-
     return(
-        <>
-            <p>{searchParams.get('lat')}</p>
-            <p>{searchParams.get('lng')}</p>
-        </>
+        <Suspense>
+            <p>{Location().lat}</p>
+            <p>{Location().lng}</p>
+        </Suspense>
     );
 }
